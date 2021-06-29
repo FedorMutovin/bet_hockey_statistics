fd1b63092a0915adb15cb476575304f68bc0cfd1
F.Mutovin <mutovin9@.mail.ru>
2021-06-30 03:30:09 +0500

    'seeds'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(mobile_phone: '79823205166')
+bookmaker = Bookmaker.create!(name: '1xСтавка', country: 'Russia')
+account = Account.create!(user: user, number: '1', bookmaker: bookmaker)
+transfer = Transfer.create!(category: "deposit", amount: '100')
+league = League.create!(name: 'NHL')
+season = Season.create!(league: league, year: '2020-21')
+home_team = Team.create!(name: 'Florida Panthers', abbreviation: 'FLA', league: league)
+away_team = Team.create!(name: 'San Jose Sharks', abbreviation: 'SJS', league: league)
+regular_season = RegularSeason.create!(season: season)
+game = Game.create!(
+  league: league,
+  season: season,
+  home_team: home_team,
+  away_team: away_team,
+  gameable: regular_season,
+  date: DateTime.current,
+  score: '3:2')
+event = Event.create!(game: game, bookmaker: bookmaker, odds: '2.2', name: 'match_winner')
+bet = Bet.create!(event: event, result: 'win', amount: '100')
+operations = [{ account: account, operational: transfer },
+              { account: account, operational: bet }]
+operations.each {|operation| Operation.create!(operation)}
